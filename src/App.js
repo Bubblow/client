9cf8c3617f6e2e64eb7fec9f3217256a91a6bee7
ahzero7d1 <ayg0418@naver.com>
2024-03-07 12:26:34 +0900

    App.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,35 @@
-import React from 'react';
-import Home from './pages/home'
-import Signup from './pages/Signup';
-import Login from './pages/Login';
-import Logout from './pages/Logout';
-import Analysis from './pages/Analysis';
-import { Routes, Route } from 'react-router-dom';
-import MypageRecord from './pages/mypage/MypageRecode';
-import Education from './pages/education';
-import Model from './pages/model';
-import Question from './pages/Question';
-import Answer from './pages/answer';
+import "./App.css";
+import React from "react";
+import Home from "./pages/home";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Logout from "./pages/Logout";
+import Analysis from "./pages/Analysis";
+import { Routes, Route } from "react-router-dom";
+import MypageRecord from "./pages/mypage/MypageRecode";
+import Education from "./pages/education";
+import Model from "./pages/model";
+import Question from "./pages/Question";
+import Answer from "./pages/answer";
+import MyHeader from "./components/MyHeader";
 
 function App() {
   return (
-    <div className='App'>
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/signup" element={<Signup />}></Route>
-          <Route path="/login" element={<Login />}></Route>
-          <Route path="/logout" element={<Logout />}></Route>
-          <Route path="/question" element={<Analysis />}></Route>
-          <Route path="/answer" element={<Answer />}></Route>
-          <Route path="/mypage" element={<MypageRecord />}></Route>
-          <Route path="/education" element={<Education />}></Route>
-          <Route path="/model" element={<Model />}></Route>
-          {/* <PostCreate /> */}
-          {/* <PostRead /> */}
-        </Routes>
+    <div className="App">
+      <MyHeader />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/logout" element={<Logout />}></Route>
+        <Route path="/question" element={<Analysis />}></Route>
+        <Route path="/answer" element={<Answer />}></Route>
+        <Route path="/mypage" element={<MypageRecord />}></Route>
+        <Route path="/education" element={<Education />}></Route>
+        <Route path="/model" element={<Model />}></Route>
+        {/* <PostCreate /> */}
+        {/* <PostRead /> */}
+      </Routes>
     </div>
   );
 }
